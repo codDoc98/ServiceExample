43abaa99ab7f389955b4b05713ffa1788f7580d0
Pragya <pragya9800@gmail.com>
2022-10-12 12:24:21 +0530

    added second fragment

--- app/src/main/java/com/example/serviceexample/MainActivity.java
+++ app/src/main/java/com/example/serviceexample/MainActivity.java
@@ -13,17 +13,41 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button startService, stopService;
+    Button bf1,bf2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Fragment fragment = new BlankFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-        ft.add(R.id.fragment1,fragment,"");
-        ft.commit();
+
+        bf1=findViewById(R.id.bt_bf1);
+        bf2=findViewById(R.id.bt_bf2);
+
+        bf1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment fragment = new BlankFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.add(R.id.fragment1,fragment,"");
+                ft.commit();
+
+            }
+        });
+        bf2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment1 = new BlankFragment2();
+                FragmentManager fragmentManager1 = getSupportFragmentManager();
+                FragmentTransaction ft1 = fragmentManager1.beginTransaction();
+                ft1.add(R.id.fragment2, fragment1, "");
+                ft1.commit();
+            }
+        });
+
+
 
 //        startService=findViewById(R.id.bt_start);
 //        stopService=findViewById(R.id.bt_stop);
